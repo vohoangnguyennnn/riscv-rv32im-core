01a7604bb2f3871bad6ff7873f4842c960f20de7
vohoangnguyennnn <251036104+nguyenvohoanggggg@users.noreply.github.com>
2026-07-27 11:46:34 +0700

    build: add RTL and unit-test source lists

---
+++ files/core.f
@@ -0,0 +1,6 @@
+core/rtl/rv32_pkg.sv
+core/rtl/rv32_mem_if.sv
+core/rtl/imm_gen.sv
+core/rtl/decoder.sv
+core/rtl/alu.sv
+core/rtl/branch_unit.sv
---
+++ files/unit.f
@@ -0,0 +1,4 @@
+tb/unit/tb_alu.sv
+tb/unit/tb_imm_gen.sv
+tb/unit/tb_decoder.sv
+tb/unit/tb_branch_unit.sv
